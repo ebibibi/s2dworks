03ffdf925d18889d58c2b585ddf70e5ec37a711c
Masahiko Ebisuda <ebibibi@gmail.com>
2019-09-24 17:23:51 +0900

    add trustedhost setting

--- CreateS2DCluster/17_AddS2DNodes.ps1
+++ CreateS2DCluster/17_AddS2DNodes.ps1
@@ -1,3 +1,4 @@
+Set-Item -Path WSMan:\localhost\Client\TrustedHosts -value "s2dnode1,s2dnode2,s2dnode3,s2dnode4" -Force
 Test-Cluster -Node s2dnode1, s2dnode2, s2dnode3, s2dnode4 -Include "Storage Spaces Direct", インベントリ, ネットワーク, システムの構成
 
 Add-ClusterNode -Name s2dnode3
